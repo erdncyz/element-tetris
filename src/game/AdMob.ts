69561b8ec9ea4b0f89c72ed8fad95e089465923a
Erdinç Yılmaz <erdncyz@gmail.com>
2026-01-08 00:49:25 +0300

    Chore: Enable AdMob test mode for development

--- src/game/AdMob.ts
+++ src/game/AdMob.ts
@@ -34,7 +34,7 @@ export class AdMobService {
                 adSize: BannerAdSize.ADAPTIVE_BANNER,
                 position: BannerAdPosition.BOTTOM_CENTER,
                 margin: 0,
-                isTesting: false
+                isTesting: true
             };
             await AdMob.showBanner(options);
         };
@@ -73,7 +73,7 @@ export class AdMobService {
         try {
             const options: AdOptions = {
                 adId: 'ca-app-pub-1271900948473545/9498821654', // Real Interstitial ID from Screenshot
-                isTesting: false
+                isTesting: true
             };
             await AdMob.prepareInterstitial(options);
         } catch (e) {
